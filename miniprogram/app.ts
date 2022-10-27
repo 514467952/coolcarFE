24713f913f76d278b74a1f31bdefe887b4ba6747
unknown <514467952@qq.com>
2022-10-27 10:24:56 +0800

    服务器生成文件后，编译成功，需要解决命名不一致问题

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,8 +1,19 @@
+import { coolcar } from "./service/proto_gen/trip_pb"
 
 // app.ts
 App<IAppOption>({
   globalData: {},
   onLaunch() {
+    wx.request({
+      url:"http://106.54.49.241:8080/trip/trip123",
+      method: "GET",
+      success: res => {
+        const getTripRes = coolcar.GetTripResponse.fromObject(res.data as object)
+        console.log(getTripRes)
+      },
+      fail: console.error,
+    })
+
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
